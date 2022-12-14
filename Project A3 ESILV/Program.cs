c2c3682e9c92addba38d018b952171893f601da4
Omegalpha44 <charlesduval44@gmail.com>
2022-12-14 15:00:58 +0100

    AJout de 3 lignes de code

--- Project A3 ESILV/Program.cs
+++ Project A3 ESILV/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! \n ceci est un test");
-Console.WriteLine("Hello, World! \n ceci est un test");
+Console.WriteLine("Hello, World! \n ceci est un test1");
+Console.WriteLine("Hello, World! \n ceci est un test2");
+Console.WriteLine("Hello, World! \n ceci est un test3");
