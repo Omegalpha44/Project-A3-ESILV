7382072821b3d853e03fa00f155c4c19489e3a25
Omegalpha44 <charlesduval44@gmail.com>
2022-12-14 15:12:02 +0100

    Ajout de la classe principale

--- Project A3 ESILV/Program.cs
+++ Project A3 ESILV/Program.cs
@@ -1,7 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! \n ceci est un test");
-Console.WriteLine("Hello, World! \n ceci est un test1");
-Console.WriteLine("Hello, World! \n ceci est un test2");
-Console.WriteLine("Hello, World! \n ceci est un test3");
-//yeye
-//youyou
+﻿namespace Solution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+}
