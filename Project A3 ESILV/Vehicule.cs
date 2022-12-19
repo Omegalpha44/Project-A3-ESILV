3abf8a319dee5e6b86feb08b09ab2158d97f485b
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2022-12-19 22:32:36 +0100

    ajout des classes abstraites Personne et Vehicule

---
+++ Project A3 ESILV/Vehicule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A3_ESILV
+{
+    internal abstract class Vehicule
+    {
+        #region Champs
+        #endregion
+
+        #region Propriétés
+        #endregion
+
+        #region Constructeurs
+        #endregion
+
+        #region Méthodes
+        #endregion
+    }
+}
