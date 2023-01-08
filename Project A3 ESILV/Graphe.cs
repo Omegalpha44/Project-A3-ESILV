835dfb57b6d198bfe4fec287741025ab549e300e
Omegalpha44 <charlesduval44@gmail.com>
2023-01-08 14:11:35 +0100

    Ajout dans la classe Graphe de mon algorithme de Dikkstra. Il fonctionne en indiquant le nom du départ, le nom de l'arrivé, et une liste de liaison disponile (par exemple a,b, 2) et la valeur de la liaison.

--- Project A3 ESILV/Graphe.cs
+++ Project A3 ESILV/Graphe.cs
@@ -43,8 +43,59 @@ namespace Project_A3_ESILV
                 parents[i] = "";
                 sommetsRestants[i] = graphe.sommets[i];
             }
+            return null;
+        }
 
-           
+        public static string Dijkstra(string deb, string fin, List<Tuple<string, string, int>> arretes) // algorithme de Dijkstra
+                                                                                                        // deb = ville de départ, fin = ville d'arrivée, arretes = liste des arretes du graphe
+        {
+            List<string> sommets = new List<string>();
+            List<string> sommetsTraites = new List<string>();
+            List<string> sommetsNonTraites = new List<string>();
+            Dictionary<string, int> distances = new Dictionary<string, int>(); // distance entre le sommet de départ et le sommet en question
+            Dictionary<string, string> predecesseurs = new Dictionary<string, string>(); // sommet précédent
+            foreach (Tuple<string, string, int> arrete in arretes)
+            {
+                if (!sommets.Contains(arrete.Item1)) sommets.Add(arrete.Item1);
+                if (!sommets.Contains(arrete.Item2)) sommets.Add(arrete.Item2);
+            }
+            foreach (string sommet in sommets) // on reset les distances
+            {
+                distances.Add(sommet, int.MaxValue);
+                predecesseurs.Add(sommet, null);
+                sommetsNonTraites.Add(sommet);
+            }
+            distances[deb] = 0;
+            while (sommetsNonTraites.Count > 0)
+            {
+                string sommetActuel = sommetsNonTraites[0];
+                foreach (string sommet in sommetsNonTraites)
+                {
+                    if (distances[sommet] < distances[sommetActuel]) sommetActuel = sommet;
+                }
+                sommetsNonTraites.Remove(sommetActuel);
+                sommetsTraites.Add(sommetActuel);
+                foreach (Tuple<string, string, int> arrete in arretes)
+                {
+                    if (arrete.Item1 == sommetActuel && !sommetsTraites.Contains(arrete.Item2))
+                    {
+                        int distance = distances[sommetActuel] + arrete.Item3;
+                        if (distance < distances[arrete.Item2])
+                        {
+                            distances[arrete.Item2] = distance;
+                            predecesseurs[arrete.Item2] = sommetActuel;
+                        }
+                    }
+                }
+            }
+            string chemin = fin;
+            string predecesseur = predecesseurs[fin];
+            while (predecesseur != null)
+            {
+                chemin = predecesseur + " -> " + chemin;
+                predecesseur = predecesseurs[predecesseur];
+            }
+            return chemin;
         }
         #endregion
     }
