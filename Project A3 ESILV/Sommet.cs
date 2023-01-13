d92b5214382928950578c8afd8577ac6c82f6d30
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2023-01-13 16:41:16 +0100

    temp

---
+++ Project A3 ESILV/Sommet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A3_ESILV
+{
+    //internal class Sommet
+    //{
+    //    #region Champs
+    //    string nom;
+    //    List<Sommet> voisins;
+    //    bool traite = false;
+    //    #endregion
+
+    //    #region Propriétés
+    //    public string Nom
+    //    {
+    //        get => nom;
+    //        set => nom = value;
+    //    }
+    //    internal List<Sommet> Voisins
+    //    {
+    //        get => voisins;
+    //        set => voisins = value;
+    //    }
+    //    public bool Traite
+    //    {
+    //        get => traite;
+    //        set => traite = value;
+    //    }
+    //    #endregion
+
+    //    #region Constructeurs
+    //    public Sommet(string nom, List<Sommet> voisins)
+    //    {
+    //        this.nom = nom;
+    //        this.voisins = voisins;
+    //        this.traite = false;
+    //    }
+    //    #endregion
+
+    //    #region Méthodes
+    //    public void AjouterVoisin(Sommet s, float d)
+    //    {
+    //        this.voisins.Add(s);
+
+    //    }
+
+    //    public override string ToString()
+    //    {
+
+    //        String descr = "Noeud: " + this.nom;
+    //        descr += "\nNoeuds adjacents :";
+    //        foreach (Sommet s in this.voisins)
+    //        {
+    //            descr += s.nom + ", ";
+    //        }
+    //        descr += "\n";
+
+    //        return descr;
+    //    }
+    //    #endregion
+    //}
+}
